4cb7bd5dcfdd977364cc52ebd3b79c4622231430
Sofia Levin <sofiafontes@me.com>
2020-03-24 20:02:19 -0700

    Completed lru_cache.

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('./doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +10,10 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
-    def __init__(self, limit=10):
-        pass
+    def __init__(self, limit=10, total=0):
+        self.limit = limit
+        self.dict = {}
+        self.storage = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +23,12 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key in self.dict:
+            node = self.dict[key]
+            self.storage.move_to_front(node)
+            return node.value[1]
+        else:
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +41,12 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.dict:
+            self.dict[key].value = (key, value)
+            self.storage.move_to_front(self.dict[key])
+        else:
+            self.storage.add_to_head((key, value))
+            self.dict[key] = self.storage.head
+            if self.storage.length > self.limit:
+                del self.dict[self.storage.tail.value[0]]
+                self.storage.remove_from_tail()
